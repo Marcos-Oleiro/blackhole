36ca4888c82ba589b4838fb00d81a0f9e396c638
Marcos-Oleiro <moleiro@ymail.com>
2022-08-02 22:51:08 -0300

    commit inicial

---
+++ src/test/java/com/oleirosoftware/blackhole/BlackholeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleirosoftware.blackhole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackholeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
